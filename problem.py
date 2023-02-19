ce89f2311c8e4e84c1e458bd182d867e74be7d3c
HT MAI <huutan.mai@gmail.com>
2023-02-19 14:43:31 +0100

    Added prediction type

--- problem.py
+++ problem.py
@@ -1,7 +1,34 @@
 import os
 import pandas as pd
+import numpy as np
+
 import rampwf as rw
+from rampwf.score_types.base import BaseScoreType
+
+from prediction_types.generative import make_generative
 
 problem_title = "GAN Anime"
 
+# -----------------------------------------------------------------------------
+# Worklow element
+# -----------------------------------------------------------------------------
 
+
+# -----------------------------------------------------------------------------
+# Predictions type
+# -----------------------------------------------------------------------------
+p = 64 * 64
+
+BaseGenerative = make_generative(
+    label_names=np.arange(p))
+
+# TODO: Redefine Predictions to fit the challenge.
+
+# -----------------------------------------------------------------------------
+# Score types
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Training / testing data reader
+# -----------------------------------------------------------------------------
