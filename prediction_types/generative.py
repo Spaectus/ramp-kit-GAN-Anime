5eb924d98a9a0cf4845fb14d07b79bb3b39c85ac
HT MAI <huutan.mai@gmail.com>
2023-02-20 14:27:52 +0100

    Updated prediction types

--- prediction_types/generative.py
+++ prediction_types/generative.py
@@ -7,6 +7,20 @@ y_pred can be one or two-dimensional (for multi-target regression)
 import numpy as np
 from rampwf.prediction_types.base import BasePrediction
 
+class BaseImgGen(BasePrediction):
+    def valid_indexes(self):
+        if len(self.y_pred.shape) <= 3:
+            ValueError('y_pred.shape <= 3 is not implemented')
+        elif len(self.y_pred.shape) == 4:
+            # (n_samples, channel, height, width)
+            return ~np.isnan(self.y_pred).any(axis=1)
+        else:
+            raise ValueError('y_pred.shape > 4 is not implemented')
+        
+    def check_y_pred_dimensions(self):
+        # TODO: Implement this for images.
+        pass
+
 
 def _generation_init(self, y_pred=None, y_true=None, n_samples=None,
                      fold_is=None):
@@ -48,6 +62,43 @@ def _generation_init(self, y_pred=None, y_true=None, n_samples=None,
             'Missing init argument: y_pred, y_true, or n_samples')
     self.check_y_pred_dimensions()
 
+def _generation_img_init(self, y_pred=None, y_true=None, n_samples=None,
+                     fold_is=None):
+    """Initialize a generation prediction type.
+    The input is either y_pred, or y_true, or n_samples.
+    Parameters
+    ----------
+    y_pred : a numpy array
+        representing the predictions returned by
+        problem.workflow.test_submission; 1D (single target regression)
+        or 2D (multi-target regression)
+    y_true : a numpy array
+        representing the ground truth returned by problem.get_train_data
+        and problem.get_test_data; 1D (single target regression)
+        or 2D (multi-target regression)
+    n_samples : int
+        to initialize an empty container, for the combined predictions
+    fold_is : a list of integers
+        either the training indices, validation indices, or None when we
+        use the (full) test data.
+    """
+    if y_pred is not None:
+        if fold_is is not None:
+            y_pred = y_pred[fold_is]
+        self.y_pred = np.array(y_pred)
+    elif y_true is not None:
+        if fold_is is not None:
+            y_true = y_true[fold_is]
+        self.y_pred = np.array(y_true)
+    elif n_samples is not None:
+        shape = (n_samples, self.channels, self.height, self.width)
+        self.y_pred = np.empty(shape, dtype=float)
+        self.y_pred.fill(np.nan)
+    else:
+        raise ValueError(
+            'Missing init argument: y_pred, y_true, or n_samples')
+    self.check_y_pred_dimensions()
+
 
 def make_generative(label_names=[]):
     """Creates a prediction type for generative challenges, based on regression.
@@ -65,5 +116,17 @@ def make_generative(label_names=[]):
          'n_columns': len(label_names),
          'n_columns_true': len(label_names),
          '__init__': _generation_init,
+         })
+    return Predictions
+
+def make_generative_img(height, width, label_names=[]):
+    Predictions = type(
+        'Regression',
+        (BaseImgGen,),
+        {'label_names': label_names,
+         'channels': 3,
+         'height': height,
+         'width': width,
+         '__init__': _generation_img_init,
          })
     return Predictions
--- problem.py
+++ problem.py
@@ -4,8 +4,10 @@ import numpy as np
 
 import rampwf as rw
 from rampwf.score_types.base import BaseScoreType
+from torchmetrics.image.fid import FrechetInceptionDistance
+from torchmetrics.image.kid import KernelInceptionDistance
 
-from prediction_types.generative import make_generative
+from prediction_types.generative import make_generative_img
 
 problem_title = "GAN Anime"
 
@@ -17,10 +19,14 @@ problem_title = "GAN Anime"
 # -----------------------------------------------------------------------------
 # Predictions type
 # -----------------------------------------------------------------------------
-p = 64 * 64
+width = 64
+height = 64
+channels = 3
+p = channels * height * width
 
-BaseGenerative = make_generative(
-    label_names=np.arange(p))
+BaseGenerative = make_generative_img(
+    height=height, width=width, label_names=np.arange(p)
+)
 
 # TODO: Redefine Predictions to fit the challenge.
 
@@ -28,6 +34,56 @@ BaseGenerative = make_generative(
 # Score types
 # -----------------------------------------------------------------------------
 
+# Fréchet Inception Distance (FID)
+class FID(BaseScoreType):
+    def __init__(self, name='fid_score'):
+        self.fid = FrechetInceptionDistance(reset_real_features=True)
+        self.name = name
+
+    def __call__(self, y_true, y_pred):
+        # y_true = X ; y_pred = X_gen = G(z)
+        self.fid.update(y_true, real=True)
+        self.fid.update(y_pred, real=False)
+        score = self.fid.compute()
+        return score
+
+# Kernel Inception Distance (KID)
+
+# TODO: see if we can combine both into one type of score because computing the core twice
+# is expensive.
+
+class KIDMean(BaseScoreType):
+    def __init__(self, name='kid_mean'):
+        self.kid = KernelInceptionDistance(reset_real_features=True)
+        self.name = name
+
+    def __call__(self, y_true, y_pred):
+        # y_true = X ; y_pred = X_gen = G(z)
+        self.kid.update(y_true, real=True)
+        self.kid.update(y_pred, real=False)
+        score = self.kid.compute()
+        return score[0]
+
+class KIDStd(BaseScoreType):
+    def __init__(self, name='fid_var'):
+        self.kid = KernelInceptionDistance(reset_real_features=True)
+        self.name = name
+
+    def __call__(self, y_true, y_pred):
+        # y_true = X ; y_pred = X_gen = G(z)
+        self.kid.update(y_true, real=True)
+        self.kid.update(y_pred, real=False)
+        score = self.kid.compute()
+        return score[1]
+
+
+# Inception Score
+
+score_types = [
+    # Fréchet Inception Distance
+    FID(),
+    KIDStd()
+]
 
 # -----------------------------------------------------------------------------
 # Training / testing data reader
